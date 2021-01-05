111dd6de72a779b65db0da5d06c4cfe8421db8e7
thedjziegler <thedjziegler@gmail.com>
2021-01-04 22:10:32 -0500

    descriptions

--- src/pages/blog/index.jsx
+++ src/pages/blog/index.jsx
@@ -14,9 +14,7 @@ const Blog = ({ data }) => (
       <Header />
       <SEO
         title="Blog"
-        description="I like blogging about various web technologies and other stuff related to
-          javascript and other trends like graphql, prisma etc. This blog expresses my views of various technologies
-          and scenarios I have come across in realtime."
+        description="I like bloggin about various topics of my life. If you have any questions please feel free to contact me."
         path="blog"
       />
       <SidebarWrapper>
